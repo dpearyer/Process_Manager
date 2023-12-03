c4c255f53689d7031ceeade5a4a7cdade0305ff3
hpham32 <knightpham93@gmail.com>
2023-12-03 10:25:19 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,27 @@ app.get('/getProcesses', async (req, res) => {
   }
 });
 
+app.get('/getSystemInfo', async (req, res) => {
+  try {
+    const osInfo = await si.osInfo();
+    const cpu = await si.cpu();
+    const mem = await si.mem();
+    const diskLayout = await si.diskLayout();
+
+    const systemInfo = {
+      deviceName: osInfo.hostname,
+      osType: osInfo.distro,
+      cpuType: cpu.manufacturer + ' ' + cpu.brand,
+      ram: mem.total,
+      rom: diskLayout[0].size,
+    };
+
+    res.json(systemInfo);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving system information' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
